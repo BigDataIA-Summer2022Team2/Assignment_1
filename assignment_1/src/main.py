1c90383260d2ac5efb6a5884a8a5ac8eea6933e2
Cheng <lemonsoldout@163.com>
2022-06-13 02:19:18 -0400

    test log output

--- assignment_1/src/main.py
+++ assignment_1/src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import myFunctions
+from log.logger import logger
+import os,sys,time
 
 app = FastAPI()
 
@@ -13,10 +15,10 @@ async def postCsvInfo(request : str):
     return response
 
 
-@app.post("/post/fileName/{aircraft_fileName}")
-async def postCsvInfo(request : str):
-    response = myFunctions.aircraft_fileName_request(request)
-    return response
+# @app.post("/post/fileName/{aircraft_fileName}")
+# async def postCsvInfo(request : str):
+#     response = myFunctions.aircraft_fileName_request(request)
+#     return response
 
 
 @app.get('/api/')
@@ -41,7 +43,7 @@ async def students_info(student_id: int, str = None):
 def post_msg(body:dict):
     return body
 
-@app.get("/html/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 async def html_page():
     return """
         <html lang="en">
@@ -52,7 +54,45 @@ async def html_page():
             <title>Document</title>
         </head>
         <body>
-            <h1>Hello!</h1>
+            <h1><font color = "#ff6b81">DAMG 7245 Assignment 1</font></h1>
+            <h2> <font color="#1e90ff">Team 2 members</font> <h2>
+            <h3> <font color="#222f3e">Cheng Wang NUID: 001280107</font><h3>
+            <h3> <font color="#222f3e">Meihu Qin NUID: </font><h3><h3>
+            <button><a href = "https://github.com/1207Lemon/Big-Data-Systems-Intelligence-Analytics-Labs-Summer-2022"><font color="#8999ef">jump to Github Repo</font></a></button>
+            
         </body>
         </html>
     """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @logger.catch # 异常记录装饰器、放到下面好像不行、应该是异步的关系。
+# def my_function(x, y, z):
+#     return [x,y,z]
+
+
+@app.get('/test/')
+async def root():
+    logger.info('=============== root API start ===============')
+    logger.info('URL: 127.0.0.1:/8000/test/')
+    logger.info('HTTP get')
+    # get 请求
+    logger.debug(f"这是日志！")
+    logger.info('这是 {interface} 接口, 当前时间戳为：{tiems}',interface="root", tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+    logger.error(f"这是一个 error 信息!")
+    logger.warning(f"这是一个 warning 信息!")
+
+
+    logger.info('=============== root API end ===============\n\n')
+    return {'message':'Hello World!'}
