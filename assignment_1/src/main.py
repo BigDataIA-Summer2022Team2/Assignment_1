cdd6e08ccc199fcc6e6e64fb3c00ba8f4e4720a2
Cheng <lemonsoldout@163.com>
2022-06-15 01:22:49 -0400

    add display image page

--- assignment_1/src/main.py
+++ assignment_1/src/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
+from typing import Union
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from traitlets import default
 from log.logger import logger
 import os,sys,time
 import myFunctions
@@ -12,28 +14,36 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 
-@app.get("/api/get/aircraftClassRequest/{aircraft_class}")
-async def getClassImgInfo(request : str):
+@app.get("/api/get/aircraftClassRequest/")
+async def aircraftClassRequest(request : str = Query(default="F16",max_length=5)):
+    logger.info('=============== API start ===============')
+    funcName = getattr(myFunctions.getTopNAircraftClassRequest,'__name__')
+    logger.info('Call Interface: {interface}',interface=funcName)
+    logger.info( 'Current Time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+    
     response = myFunctions.aircraftClassRequest(request)
+
+    logger.info( 'Current Time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+    logger.info('=============== API end ===============\n\n')
     return response
 
 
-@app.get("/api/get/fileNameRequest/{aircraft_fileName}")
-async def fileNameInfoRequest(request : str):
+@app.get("/api/get/fileNameRequest/")
+async def fileNameRequest(request : str = Query(default="0cd99a9ee135c7618006540f5b6d9b1b",max_length=32)):
     response = myFunctions.aircraftFileNameRequest(request)
     return response
 
 
-@app.get('/api/get/csvInfo/')
-async def getCsvFileInfo():
+@app.get('/api/get/allInfo/')
+async def getAllImgInfo():
     logger.info('=============== API start ===============')
 
-    funcName = getattr(myFunctions.getAllCsvInfo,'__name__')
-    logger.info('这是 {interface} 接口 当前时间戳为：{tiems}',interface=funcName, tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+    funcName = getattr(myFunctions.getAllInfo,'__name__')
+    logger.info('Call {interface}  Now Time：{tiems}',interface=funcName, tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
     logger.info('=============== API end ===============\n\n')
     
-    return myFunctions.getCsvInfo()
+    return myFunctions.getAllInfo()
 
 @app.get("/pandas/html/", response_class=HTMLResponse)
 async def getPandasHtmlPage():
@@ -76,17 +86,81 @@ async def home():
     funcName = getattr(myFunctions.getHomePage,'__name__')
     logger.info('Interface Name: {interface}\tNow Time: {tiems}',interface=funcName, tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
-    response = myFunctions.getHomePage()
+    logger.info('=============== API end ===============\n\n')
 
+    return myFunctions.getHomePage()
+
+
+@app.get("/image/", response_class=HTMLResponse)
+async def displayImage():
+    logger.info('=============== API start ===============')
+
+    funcName = getattr(myFunctions.showImg,'__name__')
+    logger.info('Interface Name: {interface}\tNow Time: {tiems}',interface=funcName, tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
     logger.info('=============== API end ===============\n\n')
 
+    return myFunctions.showImg()
+
+
+
+@app.get("/api/get/random/")
+async def getOneRandomImage(): # default 5 images with their info
+    
+    logger.info('=============== API start ===============')
+    funcName = getattr(myFunctions.getOneRandomImage,'__name__')
+    logger.info('Call: {interface}',interface=funcName)
+    logger.info( 'Process start: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+
+    response = myFunctions.getOneRandomImage()
+
+    logger.info( 'Process end: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+    logger.info('=============== API end ===============')
     return response
 
 
 
 
+
+
+
+
+@app.get("/api/get/oneImageAircrafts/")
+async def getOneImeNumAircrafts():
+    pass
+
+
+
+
+
+
+
+
+
+
 ############################### DEMO ##################################
+# @app.get("/testtttt/")
+# async def read_items(aircraft_class: str = Query(default="e.g. F16",max_length=16)):
+#     #results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    
+#     # test
+#     #aircraft_class: int = Path(title="The ID of the item to get"),
+#     results = {"aircraft_class": aircraft_class}
+#     return results
+
+
+
+# @app.get("/testttttttt/{item_id}")
+# async def read_items(
+#     item_id: int = Path(title="The ID of the item to get"),
+#     q: Union[str, None] = Query(default=None, alias="item-query"),
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
 # @app.get('/test/')
 # async def root():
 #     logger.info('=============== API start ===============')
@@ -112,7 +186,6 @@ async def home():
 #     return {"item_id": student_id}
 
 
-# # ?Demo
 # @app.post("/posts/")
 # def post_msg(body:dict):
 #     return body
