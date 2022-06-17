df9f33d0cb25bdc99f2502fdf0413bb2d7666fe7
Cheng <lemonsoldout@163.com>
2022-06-16 23:54:56 -0400

    update api functions

--- assignment_1/src/myFunctions.py
+++ assignment_1/src/myFunctions.py
@@ -3,7 +3,10 @@ import os
 import re
 import json
 import random
+from httplib2 import Credentials
 import numpy as np
+from json import load
+from boto3 import client
 from typing import Union
 from pydantic import BaseModel
 import base64
@@ -377,8 +380,22 @@ def showRandomImg():
 
 
 
-
-
+def getS3BucketBody(combined_csv):
+    bucket=credentials['aws_s3_bucket_name']
+    
+    #check image or csv
+    
+    key="csv/" + combined_csv
+    abs_path = os.path.dirname((os.path.abspath(__file__)))
+    csv_path = abs_path+"\\credentials\\aws_s3_credentials.json"
+    credentials = json.load(open(csv_path))
+    client = client(service_name=credentials['service_name'],
+                 aws_access_key_id=credentials['aws_access_key_id'],
+                 aws_secret_access_key=credentials['aws_secret_access_key'])
+    
+    result = client.get_object(Bucket=bucket, Key=key) 
+    body = result["Body"].read().decode()
+    return body
 
     
 
